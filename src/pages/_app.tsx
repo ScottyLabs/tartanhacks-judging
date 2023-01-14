63894ea2932360ea9df53288aae2d9dcf5900083
Gram Liu <gram.liu226@gmail.com>
2023-01-14 23:10:58 +0800

    Add static assets and header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
+import Head from "next/head";
 
 interface AppProps {
   session: Session;
@@ -13,6 +14,42 @@ interface AppProps {
 const MyApp: AppType<AppProps> = ({ Component, pageProps }) => {
   return (
     <SessionProvider session={pageProps.session}>
+      <Head>
+        <title>TartanHacks 2023 | Feb 3-4, 2023</title>
+        <meta name="title" content="TartanHacks 2023 | Feb 3-4, 2023" />
+        <meta
+          name="description"
+          content="TartanHacks is Carnegie Mellon's largest hackathon! It is a 24-hour hackathon where participants from all over the country create innovative projects."
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://tartanhacks.com/" />
+        <meta property="og:title" content="TartanHacks 2023 | Feb 3-4, 2023" />
+        <meta
+          property="og:description"
+          content="TartanHacks is Carnegie Mellon's largest hackathon! It is a 24-hour hackathon where participants from all over the country create innovative projects."
+        />
+        <meta
+          property="og:image"
+          content="https://tartanhacks.com/cover-photo-2023.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://tartanhacks.com/" />
+        <meta
+          property="twitter:title"
+          content="TartanHacks 2023 | Feb 3-4, 2023"
+        />
+        <meta
+          property="twitter:description"
+          content="TartanHacks is Carnegie Mellon's largest hackathon! It is a 24-hour hackathon where participants from all over the country create innovative projects."
+        />
+        <meta
+          property="twitter:image"
+          content="https://tartanhacks.com/cover-photo-2023.png"
+        />
+        <link rel="icon" href="https://tartanhacks.com/favicon.png" />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
