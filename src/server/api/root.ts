48521cf3988b4f42be7f68de2d7f71b00eb64289
Gram Liu <gram.liu226@gmail.com>
2023-01-20 17:26:46 -0500

    Create trpc judging headers

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter } from "./trpc";
-import { exampleRouter } from "./routers/example";
+import { judgingRouter } from "./routers/judging";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,7 @@ import { exampleRouter } from "./routers/example";
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  judging: judgingRouter,
 });
 
 // export type definition of API
