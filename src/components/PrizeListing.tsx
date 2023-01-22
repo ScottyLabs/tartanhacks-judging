435973532772259c2213b850c8da5b020d728cc8
pm3512 <aobolens@andrew.cmu.edu>
2023-01-22 16:04:03 -0500

    added descriptions to prizes

--- src/components/PrizeListing.tsx
+++ src/components/PrizeListing.tsx
@@ -6,6 +6,7 @@ export interface PrizeListingProps {
   className?: string;
   prizeName: string;
   sponsorLogo: string;
+  prizeDescription?: string
 }
 
 /**
@@ -15,16 +16,28 @@ export default function PrizeListing({
   className,
   prizeName,
   sponsorLogo,
+  prizeDescription
 }: PrizeListingProps): ReactElement {
   return (
     <div
       className={clsx(
         className,
-        "w-12/12 flex flex-row items-center gap-3 rounded-2xl bg-white py-3 px-5 drop-shadow"
+        "w-12/12 rounded-2xl bg-white py-3 px-5 drop-shadow"
       )}
     >
-      <Image src={sponsorLogo} height={32} width={32} alt="Company logo" />
-      <p>{prizeName}</p>
+      <div className="flex flex-row items-center gap-3">
+        <Image src={sponsorLogo} height={32} width={32} alt="Company logo" />
+        <p>{prizeName}</p>
+      </div>
+      {
+        prizeDescription && 
+            <details className="pt-2">
+              <summary className="text-md">Description</summary>
+              <div className="pt-2">
+                <p className="break-normal">{prizeDescription}</p>
+              </div>
+            </details>
+      }
     </div>
   );
 }
--- src/pages/judging.tsx
+++ src/pages/judging.tsx
@@ -18,14 +18,20 @@ export default function JudgingPage(props: any) {
     {
       sponsorLogo: "/sponsors/scottylabs.svg",
       prizeName: "Scott Krulcik Grand Prize",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus consequuntur exercitationem soluta, in autem maiores animi? Iure veniam consectetur cumque exercitationem blanditiis nihil provident voluptatibus ab, eaque quisquam amet excepturi.",
     },
     {
       sponsorLogo: "/sponsors/scottylabs.svg",
       prizeName: "First Penguin Award",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus consequuntur exercitationem soluta, in autem maiores animi? Iure veniam consectetur cumque exercitationem blanditiis nihil provident voluptatibus ab, eaque quisquam amet excepturi.",
     },
     {
       sponsorLogo: "/sponsors/algorand.png",
       prizeName: "Best Use of Algorand",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus consequuntur exercitationem soluta, in autem maiores animi? Iure veniam consectetur cumque exercitationem blanditiis nihil provident voluptatibus ab, eaque quisquam amet excepturi.",
     },
   ];
   return (
@@ -34,13 +40,13 @@ export default function JudgingPage(props: any) {
       <main className="flex flex-col items-center gap-5 py-5 px-2 md:px-10">
         <CloseButton />
 
-        <div className="flex max-w-2xl flex-col items-center gap-10 px-6">
+        <div className="flex max-w-xl flex-col items-center gap-10 px-6">
           <p className="w-full text-2xl font-bold sm:text-center">
             Current Project
           </p>
 
           {/* Project info */}
-          <div className="w-full grow rounded-md border-4 border-blue p-8 shadow-md">
+          <div className="grow rounded-md border-4 border-blue p-8 shadow-md">
             <div className="flex grow flex-row items-center justify-start pb-4">
               <p className="pr-10 text-xl">Table {project.table}:</p>
               <p className="text-xl font-bold text-yellow">{project.name}</p>
@@ -49,29 +55,34 @@ export default function JudgingPage(props: any) {
               <p className="pr-10 text-xl">Team:</p>
               <p className="text-xl font-bold text-yellow">{project.team}</p>
             </div>
-            <details>
+            <div className="overflow-hidden">
+            <details className="inline-block">
               <summary className="text-xl">Project description</summary>
               <div>
                 <p className="break-normal">{project.description}</p>
               </div>
             </details>
+            </div>
           </div>
-          <Button text="Get Next Project" className="w-full" />
+          {isFirstProject ? (
+            <>
+              <Button text="Get Next Project" className="px-20"/>
 
-          {/* Prizes */}
-          <p className="mt-5">You are judging the following prizes</p>
-          <div className="w-12/12 flex flex-col gap-5">
-            {prizes.map((prize, i) => {
-              return (
-                <button>
-                  <PrizeListing
-                    sponsorLogo={prize.sponsorLogo}
-                    prizeName={prize.prizeName}
-                  />
-                </button>
-              );
-            })}
-          </div>
+              {/* Prizes */}
+              <p className="mt-5">You are judging the following prizes</p>
+              <div className="flex flex-col gap-5">
+                {prizes.map((prize, i) => {
+                  return (
+                    <PrizeListing
+                      sponsorLogo={prize.sponsorLogo}
+                      prizeName={prize.prizeName}
+                      prizeDescription={prize.description}
+                    />
+                  );
+                })}
+              </div>
+            </>
+          ) : null}
         </div>
       </main>
     </>
