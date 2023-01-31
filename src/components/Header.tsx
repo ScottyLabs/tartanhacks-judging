c2da6823fc6c9f5d55a7887ada4d7924069cb7bc
Gram Liu <gram.liu226@gmail.com>
2023-01-30 23:09:13 -0500

    Fix header layout on mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,22 +10,19 @@ export default function Header({ hideAuth = false }: Props) {
   return (
     <header>
       <nav className="navbar navbar-expand-lg bg-gradient-to-b from-blue to-purple py-2 shadow-md">
-        <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between">
+        <div className="mx-auto flex max-w-screen-xl items-center justify-between px-2">
+          <div id="mobile-menu-2">
+            <Link href="/">
+              <Logo className="h-12" />
+            </Link>
+          </div>
           {!hideAuth && (
-            <div className="flex items-center lg:order-2">
+            <div className="">
               <a href="#" onClick={() => void signOut()} className="text-white">
                 Sign out
               </a>
             </div>
           )}
-          <div
-            className="hidden w-full items-center justify-between lg:order-1 lg:flex lg:w-auto"
-            id="mobile-menu-2"
-          >
-            <Link href="/">
-              <Logo className="h-12" />
-            </Link>
-          </div>
         </div>
       </nav>
     </header>
