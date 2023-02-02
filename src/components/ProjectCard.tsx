f1fad580012f25aae50a5c763131da3fa7d1a78c
Gram Liu <gram.liu226@gmail.com>
2023-02-02 01:56:16 -0500

    Fix font size of project card

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -6,6 +6,9 @@ interface Props {
   isFetching: boolean;
 }
 
+/**
+ * A summary of project information displayed on the judging screen
+ */
 export default function ProjectCard({
   project,
   isFetching,
@@ -13,24 +16,24 @@ export default function ProjectCard({
   return (
     <div
       className={clsx(
-        "grow rounded-md border-4 border-blue p-8 shadow-md",
+        "w-full rounded-md border-4 border-blue p-4 shadow-md",
         isFetching ? "animate-pulse" : null
       )}
     >
-      <div className="flex grow flex-row items-center justify-start pb-4">
-        <p className="pr-10 text-xl font-bold">Project:</p>
+      <div className="flex grow flex-row items-center justify-between pb-4">
+        <p className="pr-5 text-lg font-bold">Project:</p>
         <p className="text-xl font-bold text-yellow">{project.name}</p>
       </div>
-      <div className="flex grow flex-row items-center justify-start pb-5">
-        <p className="pr-10 text-xl font-bold">Team:</p>
+      <div className="flex grow flex-row items-center justify-between pb-5">
+        <p className="pr-5 text-lg font-bold">Team:</p>
         <p className="text-xl font-bold text-yellow">{project.team}</p>
       </div>
-      <div className="flex grow flex-row items-center justify-start pb-4">
-        <p className="pr-10 text-xl font-bold">Location:</p>
+      <div className="flex grow flex-row items-center justify-between pb-4">
+        <p className="pr-5 text-lg font-bold">Location:</p>
         <p className="text-xl font-bold text-yellow">{project.location}</p>
       </div>
       <div className="overflow-hidden">
-        <p className="pr-10 text-xl font-bold">Description:</p>
+        <p className="pr-5 text-lg font-bold">Description:</p>
         <div>
           <p className="text-md break-normal">{project.description}</p>
         </div>
