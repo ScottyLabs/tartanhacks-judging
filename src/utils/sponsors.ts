104a94074d97bba51d0cb75570b458ddc0a65aae
Gram Liu <gram.liu226@gmail.com>
2023-02-04 14:46:13 -0500

    Fix logo assignment

--- src/utils/sponsors.ts
+++ src/utils/sponsors.ts
@@ -1,10 +1,13 @@
 const sponsorLogoMapping = {
-  Algorand: "/sponsors/algorand.png",
+  "Algorand Foundation": "/sponsors/algorand.png",
   ScottyLabs: "/sponsors/scottylabs.svg",
 } as const;
 
 export type Sponsor = keyof typeof sponsorLogoMapping;
 
 export function getSponsorLogoUrl(sponsor: Sponsor): string {
-  return sponsorLogoMapping[sponsor ?? "ScottyLabs"];
+  return (
+    sponsorLogoMapping[sponsor ?? "ScottyLabs"] ??
+    sponsorLogoMapping["ScottyLabs"]
+  );
 }
