873d09753255b1493f3bd78f9214f7446d2380e3
Gram Liu <gram.liu226@gmail.com>
2023-02-04 15:00:40 -0500

    Add other prize sponsors

--- src/utils/sponsors.ts
+++ src/utils/sponsors.ts
@@ -1,6 +1,10 @@
 const sponsorLogoMapping = {
   "Algorand Foundation": "/sponsors/algorand.png",
   ScottyLabs: "/sponsors/scottylabs.svg",
+  "Sandia National Laboratories": "/sponsors/sandia.png",
+  "Design for America": "/sponsors/dfa.png",
+  "Google Cloud": "/sponsors/gcp.png",
+  "Wolfram Language": "/sponsors/wolfram.png",
 } as const;
 
 export type Sponsor = keyof typeof sponsorLogoMapping;
