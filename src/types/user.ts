f5fa5c0f04d882858821a46a9a723aafe4cd9575
Anuda Weerasinghe <anuda@live.com>
2024-03-09 19:05:08 -0500

    implements external auth case in next auth callback

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,14 +1,6 @@
+import { UserType } from "@prisma/client";
+
 export interface HelixUser {
-  admin: boolean;
-  judge: boolean;
-  company?: {
-    name: string;
-  };
-  status: string;
-  _id: string;
-  email: string;
-  createdAt: string;
-  updatedAt: string;
-  __v: number;
-  token: string;
+  userType: UserType;
+  isAdmin: boolean;
 }
