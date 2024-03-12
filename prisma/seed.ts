3e5917e3deaaa411f63d1133c7a4b8528261716c
pm3512 <aobolens@andrew.cmu.edu>
2024-03-12 16:46:21 -0400

    adds settings to seeding script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,17 +1,16 @@
-import { PrismaClient, UserType } from '@prisma/client';
-import { generateJWT } from '../src/utils/auth';
+import { PrismaClient, Settings, UserType } from "@prisma/client";
+import { generateJWT } from "../src/utils/auth";
 const prisma = new PrismaClient();
-
 
 async function main() {
   // Check if an admin user already exists
   const adminUser = await prisma.user.findFirst({
-    where: { isAdmin: true},
+    where: { isAdmin: true },
   });
 
   if (!adminUser) {
     const jwtSecret = process.env.JWT_SECRET as string;
-    const email = process.env.ADMIN_EMAIL as string
+    const email = process.env.ADMIN_EMAIL as string;
     const token = generateJWT(email, jwtSecret);
     // Create a new admin user if it doesn't exist
     await prisma.user.create({
@@ -23,7 +22,25 @@ async function main() {
     });
     console.log(`Admin user created, token: ${token}`);
   } else {
-    console.log('Admin user already exists');
+    console.log("Admin user already exists");
+  }
+
+  const settings = await prisma.settings.findFirst();
+
+  if (!settings) {
+    const defaultSettings = {
+      authMode: "LOCAL",
+      authUrl: "",
+      judgingDeadline: new Date(),
+      minVisits: 0,
+      sigmaInit: 1.0,
+      getTeamUrl: "",
+      serviceToken: "",
+    } as Settings;
+
+    await prisma?.settings.create({
+      data: defaultSettings,
+    });
   }
 }
 
