45639bba3a0bfd84c16bd6eba2d6539ac957ea80
Anuda Weerasinghe <anuda@live.com>
2024-03-13 18:06:04 -0400

    changes magicLinkToken jwt to expire in 3m

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -3,15 +3,13 @@ import jwt from "jsonwebtoken";
 interface JWTPayload {
   email: string;
   iat: number; // Issued at time
-  exp: number; // Expiration time
 }
-export function generateJWT(email: string, secret: string): string {
+export function generateMagicLinkToken(email: string, secret: string): string {
   const payload: JWTPayload = {
     email,
     iat: Math.floor(Date.now() / 1000), // Get the current time in seconds
-    exp: Number.MAX_VALUE,
   };
-  return jwt.sign(payload, secret);
+  return jwt.sign(payload, secret, { expiresIn: "3m" }); // Expires in 3 minutes
 }
 
 export function decodeJWT(token: string, secret: string): JWTPayload {
