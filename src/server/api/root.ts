1ce74e7866b8dcbdab021a2f153e847997749cdd
Anuda Weerasinghe <anuda@live.com>
2024-03-12 20:14:38 -0400

    implements resend magic link trpc endpoint

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { createTRPCRouter } from "./trpc";
 import { judgingRouter } from "./routers/judging";
 import { prizesRouter } from "./routers/prizes";
+import { authRouter } from "./routers/auth";
 
 /**
  * This is the primary router for your server.
@@ -10,6 +11,7 @@ import { prizesRouter } from "./routers/prizes";
 export const appRouter = createTRPCRouter({
   judging: judgingRouter,
   prizes: prizesRouter,
+  auth: authRouter,
 });
 
 // export type definition of API
