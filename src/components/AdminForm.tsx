4161949b214b6b4c8b0d715fd2653ee4a62dccc6
pm3512 <aobolens@andrew.cmu.edu>
2024-03-14 00:16:16 -0400

    refactors admin page

---
+++ src/components/AdminForm.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import AdminSettings from "./AdminSettings";
+import Spinner from "./Spinner";
+
+export default function AdminForm() {
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [submittedSignal, setSubmittedSignal] = useState(false);
+
+  return (
+    <>
+      {isLoading && <Spinner />}
+      <div className={isLoading ? "hidden" : "flex w-96 flex-col gap-8"}>
+        <AdminSettings
+          setError={setError}
+          setIsLoading={setIsLoading}
+          submittedSignal={submittedSignal}
+        />
+        <button
+          className="btn rounded-md bg-purple px-3 py-1 text-white"
+          onClick={() => {
+            setSubmittedSignal(!submittedSignal);
+          }}
+        >
+          Save
+        </button>
+      </div>
+      {!isLoading && error && <p className="text-red-500">{error}</p>}
+    </>
+  );
+}
--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -2,36 +2,30 @@ import { useState } from "react";
 import Header from "../components/Header";
 import Spinner from "../components/Spinner";
 import AdminSettings from "../components/AdminSettings";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "../../@/components/ui/tabs";
+import AdminForm from "../components/AdminForm";
 
 export default function Admin() {
-  const [error, setError] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [submittedSignal, setSubmittedSignal] = useState(false);
-
   return (
     <>
       <Header />
       <main className="flex flex-col items-center gap-5 py-5 px-2 md:px-10">
         <h1 className="text-3xl font-bold">Admin</h1>
-        {isLoading && <Spinner />}
-        <div className={isLoading ? "hidden" : "w-96"}>
-          <AdminSettings
-            setError={setError}
-            setIsLoading={setIsLoading}
-            submittedSignal={submittedSignal}
-          />
-        </div>
-        <div className="flex gap-2">
-          <button
-            className="btn m-auto rounded-md bg-purple px-3 py-1 text-white"
-            onClick={() => {
-              setSubmittedSignal(!submittedSignal);
-            }}
-          >
-            Save
-          </button>
-        </div>
-        {!isLoading && error && <p className="text-red-500">{error}</p>}
+        <Tabs defaultValue="settings" className="w-96">
+          <TabsList className="pb-8">
+            <TabsTrigger value="settings">Settings</TabsTrigger>
+            <TabsTrigger value="users">Users</TabsTrigger>
+          </TabsList>
+          <TabsContent value="settings">
+            <AdminForm />
+          </TabsContent>
+          <TabsContent value="users"></TabsContent>
+        </Tabs>
       </main>
     </>
   );
