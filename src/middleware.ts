6cda32b32ecf4c1a93d4d3d16f845a59d562af6d
Anuda Weerasinghe <anuda@live.com>
2024-03-14 15:03:03 -0400

    removes console.log in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import { withAuth } from "next-auth/middleware";
 export default withAuth(
   // `withAuth` augments your `Request` with the user's token.
   function middleware(req) {
-    console.log(req.nextauth.token);
+    return;
   },
   {
     callbacks: {
