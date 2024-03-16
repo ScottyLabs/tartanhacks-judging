6a1c2401c4c473b6d2d59db20d0f868a0629bd30
pm3512 <aobolens@andrew.cmu.edu>
2024-03-16 14:00:18 -0400

    add prize update endpoints

--- src/server/api/routers/prizes.ts
+++ src/server/api/routers/prizes.ts
@@ -1,14 +1,15 @@
+import { z } from "zod";
 import authMiddleware from "../middleware/authMiddleware";
 
 import { createTRPCRouter, publicProcedure } from "../trpc";
-
-const protectedProcedure = publicProcedure.use(authMiddleware);
+import { PrizeCategory } from "@prisma/client";
+import { syncJudgePrizes } from "./judgePrizeUtils";
 
 export const prizesRouter = createTRPCRouter({
   /**
    * Get all prizes for the event
    */
-  getPrizes: protectedProcedure.query(async ({ ctx }) => {
+  getPrizes: publicProcedure.query(async ({ ctx }) => {
     const prizes = await ctx.prisma.prize.findMany({
       orderBy: [
         {
@@ -18,4 +19,47 @@ export const prizesRouter = createTRPCRouter({
     });
     return prizes;
   }),
+
+  //TODO make admin only
+  putPrizes: publicProcedure.input(
+    z.object({
+      generalPrizes: z.array(z.object({
+        name: z.string(),
+        description: z.string(),
+      })),
+      specialPrizes: z.array(z.object({
+        name: z.string(),
+        description: z.string(),
+      })),
+    }),
+  ).mutation(async ({ ctx, input }) => {
+    const { generalPrizes, specialPrizes } = input;
+    // insert prizes into the database
+    await ctx.prisma.prize.createMany({
+      data: [
+      ...generalPrizes.map((prize) => ({
+        name: prize.name,
+        description: prize.description,
+        category: PrizeCategory.GENERAL,
+      })),
+      ...specialPrizes.map((prize) => ({
+        name: prize.name,
+        description: prize.description,
+        category: PrizeCategory.SPECIAL,
+      })),
+      ],
+    });
+    // assign general prizes to all judges
+    await syncJudgePrizes(ctx.prisma);
+  }),
+
+  deletePrize: publicProcedure
+    .input(z.string().uuid())
+    .mutation(async ({ ctx, input: id }) => {
+      await ctx.prisma.prize.delete({
+        where: {
+          id,
+        },
+      });
+    }),
 });
