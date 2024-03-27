5ca74440446ced411341fc4b44338467bc984cfd
Anuda Weerasinghe <anuda@live.com>
2024-03-27 00:38:17 -0400

    fixes prize/project relations in prisma schema

--- src/pages/submit-project.tsx
+++ src/pages/submit-project.tsx
@@ -1,13 +1,51 @@
 import type { NextPage } from "next";
+import { api } from "../utils/api";
+import { useState } from "react";
+import Header from "../components/Header";
+import type { Project } from "../";
 
 const SubmitProject: NextPage = () => {
-  const [teamName, setTeamName] = useState("");
-  const [projectName, setProjectName] = useState("");
-  const [description, setDescription] = useState("");
-  const [githubUrl, setGithubUrl] = useState("");
-  const [otherResources, setOtherResources] = useState("");
-  const [teamMembers, setTeamMembers] = useState<string[]>([]);
-  return <></>;
+  const {
+    isFetching,
+    data: savedProject,
+    refetch,
+  } = api.projects.getUserProject.useQuery();
+
+  const [teamName, setTeamName] = useState(savedProject?.teamName);
+  const [projectName, setProjectName] = useState(savedProject?.name);
+  const [description, setDescription] = useState(savedProject?.description);
+  const [githubUrl, setGithubUrl] = useState(savedProject?.githubUrl);
+  const [otherResources, setOtherResources] = useState(
+    savedProject?.otherResources
+  );
+  const [teamMembers, setTeamMembers] = useState(savedProject?.teamMembers);
+
+  const { isLoading, mutate: saveProject } =
+    api.projects.saveProject.useMutation({
+      onSuccess: async () => {
+        await refetch();
+      },
+    });
+
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      <main className="flex flex-col items-center gap-5 py-5 px-2 md:px-10">
+        <div className="w-12/12 flex flex-col items-center justify-center gap-3">
+          <h2 className="text-2xl font-bold">Submit your project</h2>
+          <input
+            type="text"
+            placeholder="Search by email or role"
+            value={project?.teamName}
+            onChange={(e) =>
+              setProject({ ...project, teamName: e.target.value })
+            }
+            className="rounded-md border-2 border-purple p-2"
+          />
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default SubmitProject;
